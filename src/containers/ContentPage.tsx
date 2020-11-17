6fbb6901ce308afd667aec8dbb4ddc7a3931e2d0
bastiion <graviola@posteo.de>
2020-11-17 17:13:42 +0100

    remove COntentPage

--- src/containers/ContentPage.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { withRouteData } from 'react-static';
-import { ContentPage } from 'react-static-plugin-markdown';
-
-
-export default withRouteData(ContentPage)
--- static.config.js
+++ static.config.js
@@ -1,4 +1,3 @@
-import axios from 'axios'
 import path from 'path'
 import fs from 'fs'
 // import { Post } from './types'
