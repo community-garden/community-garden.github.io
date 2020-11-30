9215196a66758dd2d442aa990240ac22008823af
Philipp Michael <pminf@posteo.de>
2020-11-30 20:23:22 +0100

    output post translations in separate lists

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -8,18 +8,24 @@ import { post2title } from 'containers/Post'
 export default () => {
   const { posts }: { posts: Post[] } = useRouteData()
 
+  const postListItem = ({post}) => (
+    <li key={post.id} style={{listStyle:'none', fontSize: '1.5rem'}}>
+      <Link to={`/blog/post/${post.id}/`}><i className="icon angle right"></i> { post2title(post) }</Link>
+    </li>
+  );
+
   return (
     <div>
       <Helmet>
-        <title>Pergola | Recent Posts</title>
+        <title>Pergola | Blog</title>
       </Helmet>
-      <h1>Recent Posts</h1>
+      <h2>Recent Posts</h2>
       <ul style={{padding: 0}}>
-        {posts.map(post => (
-          <li key={post.id} style={{listStyle:'none', fontSize: '1.5rem'}}>
-            <Link to={`/blog/post/${post.id}/`}><i className="icon angle right"></i> { post2title(post) }</Link>
-          </li>
-        ))}
+        {posts.filter(post => post.lang == 'EN').map(post => postListItem({post}))}
+      </ul>
+      <h2>Aktuelle Posts</h2>
+      <ul style={{padding: 0}}>
+        {posts.filter(post => post.lang == 'DE').map(post => postListItem({post}))}
       </ul>
     </div>
   )
--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,7 @@
 export interface Post {
+  basename: string
   body: string
   id: number
+  lang: string
   title: string
 }
