fa66831d43a933da52973682808d326a67781cf1
bastiion <graviola@posteo.de>
2021-01-04 09:59:13 +0100

    adding deply script

--- deploy.sh
+++ deploy.sh
@@ -4,17 +4,21 @@ CURRENT_SHA=$(git rev-parse --short HEAD)
 
 echo "delete dist ..." && sleep 2
 rm -R dist
-echo "Checkout gh-pages branch..." && sleep 2
-git clone --single-branch --branch gh-pages git@github.com:community-garden/community-garden.github.io.git dist
-echo "Clearing dist directory..." && sleep 2
-rm -R dist/*
 echo "Building pages..." && sleep 2
-npx yarn run build
-cd dist || exit
+npx yarn run build || exit
+echo "Checkout gh-pages branch..." && sleep 2
+git clone --single-branch --branch gh-pages git@github.com:community-garden/community-garden.github.io.git gh-pages
+echo "Clearing gh-pages directory..." && sleep 2
+rm -R gh-pages/*
+echo "copy new dist to gh-pages..."
+cp -R dist/* gh-pages/
+cd gh-pages || exit
 echo "commit new snapshot..." && sleep 2
+git add *
 git commit -am "update pages to commit ${CURRENT_SHA}"
 echo "push to github..." && sleep 2
 git push
 echo "done!"
 cd - || exit
+rm -Rf gh-pages
 
