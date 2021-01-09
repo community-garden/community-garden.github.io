c1b6419977ddc31bd8829d9c1c98d3e9462cbedd
bastiion <graviola@posteo.de>
2021-01-09 12:38:43 +0100

    update pages to commit 3134dd9

---
+++ templates/__react_static_root__/src/containers/Markdown.8b5dce52.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{154:function(e,n,t){"use strict";t.r(n);var a=t(0),c=t.n(a),r=t(65),u=t(267),o=t.n(u);n.default=function(){var e=Object(r.useRouteData)().content;return c.a.createElement("div",null,c.a.createElement(o.a,{children:e}))}}}]);
